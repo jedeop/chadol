9677f6c0ebfb8813114d4b214ef685ce107350b1
jwp0116 <cong0116@naver.com>
2020-04-18 13:24:56 +0900

    remove router / fix electron-store

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
     <AppHeader />
-    <router-view></router-view>
+    <WindowList :windows="windows" @create-window="createWindow"  @delete-window="deleteWindow" @edit-window="editWindow" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue, Watch } from 'vue-property-decorator'
 import AppHeader from '@/components/AppHeader.vue'
+import WindowList from '@/components/WindowList.vue'
+import { Window } from '@/types/window'
+import Store from 'electron-store'
+const store = new Store()
 
 @Component({
   components: {
-    AppHeader
+    AppHeader,
+    WindowList
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private windows: Window[] = []
+
+  generateID () {
+    return Math.random().toString(36).substr(2, 9)
+  };
+
+  createWindow (url: string): void {
+    this.windows.push({
+      id: this.generateID(),
+      name: '영상',
+      url
+    })
+  }
+
+  deleteWindow (index: number): void {
+    this.windows.splice(index, 1)
+  }
+
+  editWindow (index: number, name: string): void {
+    this.windows[index].name = name
+  }
+
+  @Watch('windows', { deep: true })
+  onWindowsUpdated (data: Window[]) {
+    store.set('windows', data)
+  }
+
+  mounted () {
+    this.windows = store.get('windows', [])
+  }
+}
 </script>
 
 <style lang="less">
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import App from './App.vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faExternalLinkAlt, faWrench, faPlus, faTimes, faCheck, faTrash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import router from './router'
 
 library.add(faExternalLinkAlt, faWrench, faPlus, faTimes, faCheck, faTrash)
 
@@ -12,6 +11,5 @@ Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount('#app')
